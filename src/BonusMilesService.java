ce4114f0b81606a22db17499e7ca34ffbc015d28
Elvira Semenova <shalopayka@gmail.com>
2020-11-05 09:10:20 +0300

    changed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int bonusDivider = 20;
-        return price/bonusDivider;
-}
+        return price / bonusDivider;
+    }
 }
 
